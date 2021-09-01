2019f850ba080b100638a12908b0cab85662a5e9
Hung-Chou Tai <hungchou.tai@zegal.com>
2021-09-01 09:13:16 +0800

    updated logger to not use bunyan

--- src/commons/logger/app.js
+++ src/commons/logger/app.js
@@ -1,41 +1,7 @@
-const bunyan = require("bunyan");
-const { name, version } = require("../../../package.json");
-
-let _logger = {};
-
-let streams = [];
-streams.push(
-  {
-    level: "debug",
-    stream: process.stdout,
-  },
-  {
-    level: "info",
-    path: process.env.LOGFILE,
-  },
-  {
-    level: "error",
-    path: process.env.LOGFILE,
-  }
-); //log to stdout
-
-_logger.log = bunyan.createLogger({ name, version, streams: streams });
-
-// child logs
-_logger.getChildLogger = function (componentName) {
-  return this.log.child({
-    component: componentName,
-  });
+const logger = {
+  // Implement logger here
+  info: console.info,
+  debug: console.debug,
+  error: console.error,
 };
-
-_logger.ipFrom = function (req) {
-  return (
-    req.params.ip ||
-    req.headers["x-original-forwarded-for"] ||
-    req.headers["x-forwarded-for"] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress
-  );
-};
-
-module.exports = _logger;
+module.exports = logger;
